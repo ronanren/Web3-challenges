209fe6535308b5e1c388747c7038ae060cf175bf
ronanren <renouxpro@gmail.com>
2023-05-20 18:36:08 +0200

    update lib.rs

--- CryptoZombies/MultiversX/lib.rs
+++ CryptoZombies/MultiversX/lib.rs
@@ -24,12 +24,27 @@ pub trait ZombiesContract {
         });
     }
 
+    #[view]
+    fn generate_random_dna(&self) -> u64 {
+        let mut rand_source = RandomnessSource::new();
+        let dna_digits = self.dna_digits().get();
+        let max_dna_value = u64::pow(10u64, dna_digits as u32);
+        rand_source.next_u64_in_range(0u64, max_dna_value)
+    }
+
+    #[endpoint]
+    fn create_random_zombie(&self, name: ManagedBuffer) {
+        let rand_dna = self.generate_random_dna();
+        self.create_zombie(name, rand_dna);
+    }
+
     #[storage_mapper("dna_digits")]
     fn dna_digits(&self) -> SingleValueMapper<u8>;
 
     #[storage_mapper("zombies_count")]
     fn zombies_count(&self) -> SingleValueMapper<usize>;
 
+    #[view]
     #[storage_mapper("zombies")]
-    fn zombies(&self, id: usize) -> SingleValueMapper<Zombie<Self::Api>>;
+    fn zombies(&self, id: &usize) -> SingleValueMapper<Zombie<Self::Api>>;
 }
