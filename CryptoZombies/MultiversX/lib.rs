c34d9a2bf7895ac344afe49856930d907117bf42
ronanren <renouxpro@gmail.com>
2023-07-07 22:03:55 +0200

    update rust sc

--- CryptoZombies/MultiversX/lib.rs
+++ CryptoZombies/MultiversX/lib.rs
@@ -18,11 +18,13 @@ pub trait ZombiesContract {
     self.zombies_count().set(1usize);
   }
 
-  fn create_zombie(&self, name: ManagedBuffer, dna: u64) {
+  fn create_zombie(&self, owner: ManagedAddress, name: ManagedBuffer, dna: u64) {
     self.zombies_count().update(|id| {
       self.new_zombie_event(*id, &name, dna);
       self.zombies(id).set(Zombie { name, dna });
-      *id +=1;
+      self.owned_zombies(&owner).insert(*id);
+      self.zombie_owner(id).set(owner);
+      *id += 1;
     });
   }
 
@@ -36,8 +38,13 @@ pub trait ZombiesContract {
 
   #[endpoint]
   fn create_random_zombie(&self, name: ManagedBuffer){
+    let caller = self.blockchain().get_caller();
+    require!(
+      self.owned_zombies(&caller).is_empty(), 
+      "You already own a zombie"
+    );
     let rand_dna = self.generate_random_dna();
-    self.create_zombie(name, rand_dna);
+    self.create_zombie(caller, name, rand_dna);
   }
 
   #[event("new_zombie_event")]
