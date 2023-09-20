afed165353f98fb8ef23a6cf23ab39d4fb7509f1
ronanren <renouxpro@gmail.com>
2023-09-20 15:12:44 +0200

    add learnweb3

--- LearnWeb3/EthereumDeveloperDegree/12-IndexingDataUsingTheGraphsIndexer/README.md
+++ LearnWeb3/EthereumDeveloperDegree/12-IndexingDataUsingTheGraphsIndexer/README.md
@@ -2,4 +2,20 @@
 
 Link: https://learnweb3.io/degrees/ethereum-developer-degree/junior/indexing-data-using-the-graphs-indexer/
 
-## Requirements
+## Steps
+
+1. Create subgraphs: https://thegraph.com/hosted-service
+```bash	
+npm install -g @graphprotocol/graph-cli
+graph init --contract-name RandomWinnerGame --product hosted-service ronanren/learnweb3  --from-contract 0xD0fDCe43Cc8954981bf3Cd5e64fF6aa1F1e52bC4 --abi ./abi.json --network mumbai graph
+```
+2. Get the access token from https://thegraph.com/hosted-service/dashboard
+3. Deploy the subgraph
+```bash
+graph auth --product hosted-service ACCESS_TOKEN
+cd graph
+npm run deploy
+```
+
+Deployed to https://thegraph.com/explorer/subgraph/ronanren/learnweb3
+Queries (HTTP): https://api.thegraph.com/subgraphs/name/ronanren/learnweb3
---
+++ LearnWeb3/EthereumDeveloperDegree/12-IndexingDataUsingTheGraphsIndexer/hardhat/scripts/deploy.js
@@ -0,0 +1,38 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
+
+async function main() {
+  // deploy the contract
+  const randomWinnerGame = await hre.ethers.deployContract(
+    "RandomWinnerGame",
+    [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE]
+  );
+
+  await randomWinnerGame.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("Verify Contract Address:", randomWinnerGame.target);
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(30000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: randomWinnerGame.target,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+  });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
